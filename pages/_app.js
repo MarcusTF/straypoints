3763e0370a90d67e94b68098a2f8fe1d0bc74c37
Marcus Fernandez <marcustfernandez@gmail.com>
2021-04-19 15:24:29 -0400

    favicons

--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,27 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/*
-        <title>Stray Points</title>
-        <meta name="description" content="Digressions about things." />
-        <meta property="og:title" content="Stray Points" />
-        <meta property="og:description" content="Digressions about things." />
-        <meta property="og:image" content="./header.jpg" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="stylesheet" href="https://use.typekit.net/ywt6gyu.css" /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#444444" />
+        <meta name="theme-color" content="#ffffff" />
 
         <title>Stray Points | Digressions about things.</title>
 
