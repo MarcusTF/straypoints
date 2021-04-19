ab30195b4cbfd7c123df197c141607d90e6cfba5
Marcus Fernandez <marcustfernandez@gmail.com>
2021-04-19 14:43:12 -0400

    added vercel env variable

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
 export const APP_URL =
-  process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+  process.env.NEXT_PUBLIC_APP_URL ||
+  process.env.NEXT_PUBLIC_VERCEL_URL ||
+  'http://localhost:3000'
